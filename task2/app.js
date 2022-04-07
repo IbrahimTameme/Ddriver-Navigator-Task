8b8884ed7a93e13029311127a70b99e7c7ed039d
unknown <dua.alsafasfeh@gmail.com>
2022-04-06 18:24:25 -0700

    all tasks

--- task2/app.js
+++ task2/app.js
@@ -7,11 +7,12 @@ function uppercaseX(str){
     let capstring=" ";
     console.log(words);
     for ( let i=0; i< words.length ; i++ ){
-        array1.push(words[i].charAt(0).toUpperCase()+ words[i].slice(1));
+        array1.push(words[i].charAt(0).toUpperCase()+ words[i].slice(1) +" ");
      capstring+= array1[i];
     }
     return capstring;
 }
 uppercaseX(name2);
 // console.log(uppercaseX(name2));
-document.body.innerHTML= uppercaseX(name2);
+document.getElementById("q").innerHTML = "Q: Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case."
+document.getElementById("ans").innerHTML = `Upper the first letter of "dua ibrahim alsafasfeh" ==> ${uppercaseX(name2)}.`
